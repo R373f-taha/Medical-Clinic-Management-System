ea78da33f0e30ada87fc8ca8391322f5c90b4e4e
Rahaf Taha <rahaftaha594@example.com>
2026-01-16 19:50:04 +0200

    fix conflicts between web and jwt

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Dashboard')
+@section('title', 'Employee')
 
 @section('content')
 @if(Auth::user()->hasrole('doctor'))
@@ -24,15 +24,15 @@
 @elseif(Auth::user()->hasrole('clinicManager'))
     <div class="row">
 
-        <div class="col-lg-8 col-md-12 mb-4">
+        <div class="mb-4 col-lg-8 col-md-12">
             <div class="card h-100">
-                <div class="card-header pb-0">
+                <div class="pb-0 card-header">
                     <h6>Profits During the Weeks of the Current Month</h6>
                     <p class="text-sm">
                         <span class="font-weight-bold">Weekly Profit Percentage</span>
                     </p>
                 </div>
-                <div class="card-body p-3">
+                <div class="p-3 card-body">
                     <canvas id="weekly-profit-chart" height="300"></canvas>
                 </div>
             </div>
@@ -40,17 +40,17 @@
 
         <div class="col-lg-4 col-md-12">
 
-            <div class="card mb-4">
-                <div class="card-header pb-0">
+            <div class="mb-4 card">
+                <div class="pb-0 card-header">
                     <h6>Number of Patients per Clinic</h6>
                 </div>
-                <div class="card-body p-3">
+                <div class="p-3 card-body">
                     <canvas id="patients-per-clinic-chart" height="200"></canvas>
                 </div>
             </div>
 
             <div class="card">
-                <div class="card-header pb-0">
+                <div class="pb-0 card-header">
                     <h6>Overview of Appointments</h6>
                     <p class="text-sm">Current Appointment Status</p>
                 </div>
@@ -67,30 +67,30 @@
 
     </main>
     <div class="fixed-plugin">
-        <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-            <i class="fa fa-cog py-2"> </i>
+        <a class="px-3 py-2 fixed-plugin-button text-dark position-fixed">
+            <i class="py-2 fa fa-cog"> </i>
         </a>
-        <div class="card shadow-lg ">
-            <div class="card-header pb-0 pt-3 ">
+        <div class="shadow-lg card ">
+            <div class="pt-3 pb-0 card-header ">
                 <div class="float-start">
                     <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
                     <p>See our dashboard options.</p>
                 </div>
-                <div class="float-end mt-4">
-                    <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+                <div class="mt-4 float-end">
+                    <button class="p-0 btn btn-link text-dark fixed-plugin-close-button">
                         <i class="fa fa-close"></i>
                     </button>
                 </div>
                 <!-- End Toggle Button -->
             </div>
-            <hr class="horizontal dark my-1">
-            <div class="card-body pt-sm-3 pt-0">
+            <hr class="my-1 horizontal dark">
+            <div class="pt-0 card-body pt-sm-3">
                 <!-- Sidebar Backgrounds -->
                 <div>
                     <h6 class="mb-0">Sidebar Colors</h6>
                 </div>
                 <a href="javascript:void(0)" class="switch-trigger background-color">
-                    <div class="badge-colors my-2 text-start">
+                    <div class="my-2 badge-colors text-start">
                         <span class="badge filter bg-primary active" data-color="primary"
                             onclick="sidebarColor(this)"></span>
                         <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
@@ -109,18 +109,18 @@
                     <p class="text-sm">Choose between 2 different sidenav types.</p>
                 </div>
                 <div class="d-flex">
-                    <button class="btn btn-primary w-100 px-3 mb-2 active" data-class="bg-transparent"
+                    <button class="px-3 mb-2 btn btn-primary w-100 active" data-class="bg-transparent"
                         onclick="sidebarType(this)">Transparent</button>
-                    <button class="btn btn-primary w-100 px-3 mb-2 ms-2" data-class="bg-white"
+                    <button class="px-3 mb-2 btn btn-primary w-100 ms-2" data-class="bg-white"
                         onclick="sidebarType(this)">White</button>
                 </div>
-                <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
+                <p class="mt-2 text-sm d-xl-none d-block">You can change the sidenav type just on desktop view.</p>
                 <!-- Navbar Fixed -->
                 <div class="mt-3">
                     <h6 class="mb-0">Navbar Fixed</h6>
                 </div>
                 <div class="form-check form-switch ps-0">
-                    <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed"
+                    <input class="mt-1 form-check-input ms-auto" type="checkbox" id="navbarFixed"
                         onclick="navbarFixed(this)">
                 </div>
                 <hr class="horizontal dark my-sm-4">
@@ -129,17 +129,17 @@
                 <a class="btn btn-outline-dark w-100"
                     href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard">View
                     documentation</a>
-                <div class="w-100 text-center">
+                <div class="text-center w-100">
                     <a class="github-button" href="https://github.com/creativetimofficial/soft-ui-dashboard"
                         data-icon="octicon-star" data-size="large" data-show-count="true"
                         aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
                     <h6 class="mt-3">Thank you for sharing!</h6>
                     <a href="https://twitter.com/intent/tweet?text=Check%20Soft%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard"
-                        class="btn btn-dark mb-0 me-2" target="_blank">
+                        class="mb-0 btn btn-dark me-2" target="_blank">
                         <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
                     </a>
                     <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/soft-ui-dashboard"
-                        class="btn btn-dark mb-0 me-2" target="_blank">
+                        class="mb-0 btn btn-dark me-2" target="_blank">
                         <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
                     </a>
                 </div>
